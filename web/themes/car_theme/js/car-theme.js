a93937b7a7a1170e74e316e73d35b7ae88c64bf5
lewisleeming <lewisleeming@icloud.com>
2026-04-21 10:29:04 +0100

    Cheeta-1: Project start

---
+++ web/themes/car_theme/js/car-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * car_theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.carTheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
